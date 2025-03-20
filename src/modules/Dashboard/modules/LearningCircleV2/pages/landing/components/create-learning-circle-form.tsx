84d48e52e8c5386c4b150a614c44c3ff00a3e43c
awindsr <awindsr@gmail.com>
2025-03-20 11:35:29 +0530

    fix: create learning circle

--- src/modules/Dashboard/modules/LearningCircleV2/pages/landing/components/create-learning-circle-form.tsx
+++ src/modules/Dashboard/modules/LearningCircleV2/pages/landing/components/create-learning-circle-form.tsx
@@ -105,7 +105,7 @@ export function CreateLearningCircleForm({ onClose, meetUp, onRefresh }: CreateL
       });
     }
     onClose();
-    window.location.reload();
+    // window.location.reload();
   };
 
   return (
