cd16c3f84df1168fd33d43819d53916a9cd3a7b6
Amith Abey Stephen <amithabeystephen@gmail.com>
2025-05-28 09:09:36 +0530

    add community-partners

--- src/modules/Dashboard/modules/CommPartners/CommPartners.tsx
+++ src/modules/Dashboard/modules/CommPartners/CommPartners.tsx
@@ -14,36 +14,36 @@ type Partner = {
 const CommunityPartner = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center ">
-        <div className="flex flex-col md:flex-row items-center justify-around">
-          <div className="text-center md:text-left md:w-1/3 ">
-            <p className="font-sans text-4xl font-semibold text-[#303030] md:text-6xl md:leading-tight">
+      <div className="flex flex-col items-center justify-center pt-20 md:pt-32">
+        <div className="flex flex-col md:min-h-screen md:flex-row items-center justify-around ">
+          <div className="text-center md:text-left md:w-1/3 mx-4 md:mx-0">
+            <p className="font-sans text-5xl font-semibold text-[#303030] md:text-7xl md:leading-tight">
               <span className="font-semibold text-[#5570f1]">µLearn Community</span> Partners
             </p>
-            <p className="mt-4 md:mt-4 text-base md:text-[1.1rem] w-full font-[Poppins]">
+            <p className="mt-6 md:mt-8 text-lg md:text-2xl w-full font-[Poppins] mx-2 md:mx-0">
               When a group of like-minded people come together interesting
               things take place. What if multiple Communities join their hands
               together for a common aim things get much more interesting!
             </p>
           </div>
 
-          <div >
-            <img src={fvimg} alt="" className="block  mt-0 w-[20rem] md:w-[30rem]" />
+          <div>
+            <img src={fvimg} alt="" className="block mt-0 w-[24rem] md:w-[38rem]" />
           </div>  
         </div>
 
-        <div className="bg-white  max-w-6xl">
-          <div className="lg:w-1/2 flex flex-col text-center md:text-left relative lg:left-[-2rem] ">
-            <p className="text-2xl md:text-3xl font-semibold font-sans">
+        <div className="bg-white max-w-6xl">
+          <div className="lg:w-2/3 flex flex-col text-center md:text-left relative lg:left-[-4rem] top-[2.5rem] mx-4 md:mx-0">
+            <p className="text-3xl md:text-5xl font-semibold font-sans">
               <span className="text-[#5570f1]">Community</span> Partners
             </p>
-            <p className="mt-4 md:mt-4 text-base md:text-[1.1rem] mx-8 font-[Poppins]">
+            <p className="mt-6 md:mt-8 text-lg md:text-2xl mx-2 md:mx-0 font-[Poppins]">
               µLearn has partnered with multiple communities to provide the
               peers the best resources and events to learn and up-skill
               themselves.
             </p>
           </div>
-          <div className="grid grid-cols-1 place-items-center gap-y-4 sm:grid-cols-2  md:grid-cols-3 ">
+          <div className="grid grid-cols-1 place-items-center relative top-[5rem] gap-x-6 gap-y-12 sm:grid-cols-2 md:grid-cols-3 ">
             {(partners as Partner[]).map((partner) => (
               <CommunityCard
                 key={partner.name}
@@ -51,7 +51,6 @@ const CommunityPartner = () => {
                 cimage={partner.image}
                 clink={partner.link}
                 customlink={partner.customlink}
-                type=""
               />
             ))}
           </div>
--- src/modules/Dashboard/modules/CommPartners/CommunityCard/CommunityCard.tsx
+++ src/modules/Dashboard/modules/CommPartners/CommunityCard/CommunityCard.tsx
@@ -1,36 +1,44 @@
 import React from "react";
 import styles from "./CommunityCard.module.css";
-
 import { Link } from "react-router-dom";
 
-const CommunityCard = ({ cname, cimage, clink, customlink, type }) => {
+interface CommunityCardProps {
+  cname: string;
+  cimage: string;
+  clink: string;
+  customlink?: string;
+}
+
+const CommunityCard: React.FC<CommunityCardProps> = ({
+  cname,
+  cimage,
+  clink,
+  customlink,
+}) => {
   return (
     <div className={styles.community_card_container}>
       <div className={styles.community_card}>
-        {type ||
-          (clink && clink.length > 0 && (
-            <a href={clink} target="_blank" rel="noopener noreferrer">
-              <div className={styles.box}>
-                <img src={cimage} alt="" className={styles.communityimg} />
+        {clink && clink.length > 0 && (
+          <a href={clink} target="_blank" rel="noopener noreferrer">
+            <div className={styles.box}>
+              <img src={cimage} alt="" className={styles.communityimg} />
 
-                {cname && (
-                  <span className={styles.community_interest}>{cname}</span>
-                )}
-              </div>
-            </a>
-          ))}
+              {cname && (
+                <span className={styles.community_interest}>{cname}</span>
+              )}
+            </div>
+          </a>
+        )}
         {customlink && customlink.length > 0 && (
-          <>
-            <Link to={customlink}>
-              <div className={styles.box}>
-                <img src={cimage} alt="" className={styles.communityimg} />
+          <Link to={customlink}>
+            <div className={styles.box}>
+              <img src={cimage} alt="" className={styles.communityimg} />
 
-                {cname && (
-                  <span className={styles.community_interest}>{cname}</span>
-                )}
-              </div>
-            </Link>
-          </>
+              {cname && (
+                <span className={styles.community_interest}>{cname}</span>
+              )}
+            </div>
+          </Link>
         )}
       </div>
     </div>
